e48a44e5fa08e1a96c8de72349a0918225d0c0ca
CD Santiago <slashlos@gmail.com>
2020-06-13 16:15:27 -0400

    split type, settings modules; add QL provider

--- He3/He3-QLExtension/ThumbnailProvider.swift
+++ He3/He3-QLExtension/ThumbnailProvider.swift
@@ -8,12 +8,14 @@
 
 import QuickLookThumbnailing
 
+@available(OSXApplicationExtension 10.15, *)
 class ThumbnailProvider: QLThumbnailProvider {
     
     override func provideThumbnail(for request: QLFileThumbnailRequest, _ handler: @escaping (QLThumbnailReply?, Error?) -> Void) {
-        
+		let url = request.fileURL
+		
         // There are three ways to provide a thumbnail through a QLThumbnailReply. Only one of them should be used.
-        
+        /*
         // First way: Draw the thumbnail into the current context, set up with UIKit's coordinate system.
         handler(QLThumbnailReply(contextSize: request.maximumSize, currentContextDrawing: { () -> Bool in
             // Draw the thumbnail here.
@@ -22,8 +24,6 @@ class ThumbnailProvider: QLThumbnailProvider {
             return true
         }), nil)
         
-        /*
-        
         // Second way: Draw the thumbnail into a context passed to your block, set up with Core Graphics's coordinate system.
         handler(QLThumbnailReply(contextSize: request.maximumSize, drawing: { (context) -> Bool in
             // Draw the thumbnail here.
@@ -31,10 +31,22 @@ class ThumbnailProvider: QLThumbnailProvider {
             // Return true if the thumbnail was successfully drawn inside this block.
             return true
         }), nil)
-         
+		*/
+		
         // Third way: Set an image file URL.
-        handler(QLThumbnailReply(imageFileURL: Bundle.main.url(forResource: "fileThumbnail", withExtension: "jpg")!), nil)
-        
-        */
+		do {
+			let resourceValues = try url.resourceValues(forKeys: Set([.typeIdentifierKey, URLResourceKey.isRegularFileKey]))
+			guard let isRegularFileResourceValue = resourceValues.isRegularFile else { handler(nil,nil); return }
+			guard isRegularFileResourceValue else { handler(nil,nil); return }
+			guard let fileType = resourceValues.typeIdentifier else { handler(nil,nil); return }
+			
+			if [k.kUTHe3PlayList, k.kUTHe3PlayItem].contains(fileType) {
+				let resource = fileType == k.kUTHe3PlayList ? k.listIcon : k.itemIcon
+				handler(QLThumbnailReply(imageFileURL: Bundle.main.url(forResource: resource, withExtension: "png")!), nil)
+				return
+			}
+		} catch { }
+
+		handler(nil,nil);
     }
 }
