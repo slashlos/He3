507d9bb63fe48a97e9b14f6469f2d91949e78583
CD Santiago <slashlos@gmail.com>
2020-08-03 20:21:38 -0400

    minimize log output

--- He3/He3/Support/Extensions.swift
+++ He3/He3/Support/Extensions.swift
@@ -10,6 +10,14 @@ import Foundation
 import Cocoa
 ///import CommonCrypto
 
+public func print(_ items: String..., filename: String = #file, function : String = #function, line: Int = #line, separator: String = " ", terminator: String = "\n") {
+    #if DEBUG
+        ///let pretty = "\(URL(fileURLWithPath: filename).lastPathComponent) [#\(line)] \(function)\n\t-> "
+        let output = items.map { "\($0)" }.joined(separator: separator)
+        Swift.print(/*pretty+*/output, terminator: terminator)
+    #endif
+}
+
 public func print(_ items: Any..., separator: String = " ", terminator: String = "\n") {
     #if DEBUG
     var i = items.startIndex
