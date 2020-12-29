f9126565eecaf46b78d8365a9779dc9e17c6c600
CD Santiago <slashlos@gmail.com>
2020-12-29 14:06:41 -0500

    split out run scripts

---
+++ He3/He3Script.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#  He3Script.sh
+#  He3
+#
+#  Created by Carlos D. Santiago on 12/29/20.
+#  Copyright © 2020 Carlos D. Santiago. All rights reserved.
+
+# Localize our files with BartyCroouch: https://github.com/Flinesoft/BartyCrouch
+if which bartycrouch > /dev/null; then
+    bartycrouch update -x
+    
+    bartycrouch lint -x
+else
+    echo "warning: BartyCrouch not installed, download it from https://github.com/Flinesoft/BartyCrouch"
+fi
