f4256bed04c66ba20322b8405add13c7aff8e082
CD Santiago <slashlos@gmail.com>
2021-03-14 13:29:41 -0400

    added NSImage -flipped

--- He3/He3/Support/Extensions.swift
+++ He3/He3/Support/Extensions.swift
@@ -292,7 +292,26 @@ extension Notification.Name {
 	static let tvSelectionDidChange = Notification.Name("NSTableViewSelectionDidChange")
 }
 extension NSImage {
-    
+	/// https://stackoverflow.com/a/55446093/564870
+	func flipped(flipHorizontally: Bool = false, flipVertically: Bool = false) -> NSImage {
+		let flippedImage = NSImage(size: size)
+
+		flippedImage.lockFocus()
+
+		NSGraphicsContext.current?.imageInterpolation = .high
+
+		let transform = NSAffineTransform()
+		transform.translateX(by: flipHorizontally ? size.width : 0, yBy: flipVertically ? size.height : 0)
+		transform.scaleX(by: flipHorizontally ? -1 : 1, yBy: flipVertically ? -1 : 1)
+		transform.concat()
+
+		draw(at: .zero, from: NSRect(origin: .zero, size: size), operation: .sourceOver, fraction: 1)
+
+		flippedImage.unlockFocus()
+
+		return flippedImage
+	}
+	
 	func resize(size: NSSize) -> NSImage {
 		let newImage = NSImage(size: size)
 		newImage.lockFocus()
